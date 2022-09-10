93e1101c907d38e81e84a208413219f3cd5f426d
Hasan OZER <hohoho21207@gmail.com>
2022-09-10 22:58:38 +0300

    Update process.py

--- process.py
+++ process.py
@@ -1,47 +1,63 @@
-#python3
 from multiprocessing import Process,Pipe
 import time
-
-def process(conn):
-    while True:
-        test = conn.recv()
-        if(test[0] == "status" and test[1] == "done"):
-            break
-        if(test[0] == "data"):
-            conn.send(test[1])
-
-def initialize():
-    processes = []
-    works_to_distrubute = range(1000)
+class ProcessManager:
     parent_connections = {}
-    
-    for i in range(4):
-        parent_conn, child_conn = Pipe()    
-        p = Process(target=process, name="test"+str(i), args=(child_conn,))
-        p.start()
-        processes.append(p)
-        parent_connections["test"+str(i)] = parent_conn
-    distribute_works(works_to_distrubute,parent_connections)
+    works_to_distrubute = []
+    processes = []
+    process_namespace = "proc-geny"
+    process_count = 4
+    received_data = {}
+    def __init__(self):
+        parent_connections = {}
+        works_to_distrubute = []
+        processes = []
+        process_namespace = "proc-geny"
+        process_count = 4
+        received_data = {}
+    def child_process(self,conn,proc_name):
+        while True:
+            message = conn.recv()
+            if message[0] == "status" and message[1] == "done":
+                break
+            elif message[0] == "func" and message[1]:
+                if message[2]:
+                    callback = message[1](message[2])
+                else:
+                    callback = message[1]()
+                conn.send([proc_name,callback])
+            else:
+                raise Exception("Process parse error")
+    def process(self,works_to_distrubute): #works_to_distrubute = [["func obj","arguments"], ... ]
+        for i in range(self.process_count):
+            parent_conn, child_conn = Pipe()    
+            p = Process(target=self.child_process, name=self.process_namespace+str(i), args=(child_conn,self.process_namespace+str(i)))
+            p.start()
+            self.processes.append(p)
+            self.parent_connections[self.process_namespace+str(i)] = parent_conn
+        self.distribute_works(works_to_distrubute)
+        for p in self.processes:
+            p.join()
+        return self.received_data
+    def distribute_works(self,works_to_distrubute):
+        for cursor in range(len(works_to_distrubute)):
+            conn_num = cursor%self.process_count
+            conn = self.parent_connections[self.process_namespace+str(conn_num)]
+            conn.send(["func",*works_to_distrubute[cursor]])
+            data = conn.recv()
+            self.received_data[data[0]] = data[1]
+            if(cursor == len(works_to_distrubute)-1):
+                for cursor in range(self.process_count):
+                    conn = self.parent_connections[self.process_namespace+str(cursor)]
+                    conn.send(["status","done"])
 
-    for p in processes:
-        p.join()
-def distribute_works(works_to_distrubute,parent_connections):
-    received_data = []
-    for cursor in range(len(works_to_distrubute)):
-        conn_num = cursor%4
-        conn = parent_connections["test"+str(conn_num)]
-        conn.send(["data",works_to_distrubute[cursor]])
-        data = conn.recv()
-        received_data.append(data)
-        if(cursor == len(works_to_distrubute)-1):
-            for cursor in range(4):
-                conn = parent_connections["test"+str(cursor)]
-                conn.send(["status","done"])
-    print(received_data)
-    print(len(received_data))
+def alo(za):
+    return za
 start = time.time()
+process_manager = ProcessManager()
+oleybe = process_manager.process([[alo,"asd"],[alo,"qwe"]])
 
-initialize()
+print(oleybe)
+
 end = time.time()
 print("The time of execution of above program is :",
-      (end-start) * 10**3, "ms")
+    (end-start) * 10**3, "ms")
